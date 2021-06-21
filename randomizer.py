f98759ed877070a64a67c2278d2dc5b76fa56094
1GLEBOS1 <glebxx04@gmail.com>
2021-06-21 11:20:18 +0300

    Changed pretty output

--- randomizer.py
+++ randomizer.py
@@ -6,8 +6,8 @@ CSPRNG = random.SystemRandom()
 
 class BunkerTable:
     
-    __features = [i for i in range(1, 31)]
-    __professions = [i for i in range(1, 51)]
+    __features = [i for i in range(0, 30)]
+    __professions = [i for i in range(0, 50)]
     __features_on_table = [set(), set(), set(), set(), set(), set(), set(), set()]
     __table = [[], [], [], [], [], [], [], [], [], []]
 
@@ -55,42 +55,34 @@ class BunkerTable:
                 self.__table[i].append(item)
                 self.__features_on_table[a].discard(item)
 
+    def __restore_table(self):
+        self.__features_on_table = [set(), set(), set(), set(), set(), set(), set(), set()]
+        self.__table = [[], [], [], [], [], [], [], [], [], []]
+
     def pretty_output(self):
         """
         this method returns prettyful output of bunker table
         """
 
         table = self.__table
+        features = self.all_features
 
-        output = f'''
-        Character 1:
-        {table[0][0]}, {table[0][1]}, {table[0][2]}, {table[0][3]}, {table[0][4]}, {table[0][5]}, {table[0][6]}, {table[0][7]}
-        
-        Character 2:
-        {table[1][0]}, {table[1][1]}, {table[1][2]}, {table[1][3]}, {table[1][4]}, {table[1][5]}, {table[1][6]}, {table[1][7]}
-        
-        Character 3:
-        {table[2][0]}, {table[2][1]}, {table[2][2]}, {table[2][3]}, {table[2][4]}, {table[2][5]}, {table[2][6]}, {table[2][7]}
-        
-        Character 4:
-        {table[3][0]}, {table[3][1]}, {table[3][2]}, {table[3][3]}, {table[3][4]}, {table[3][5]}, {table[3][6]}, {table[3][7]}
-        
-        Character 5:
-        {table[4][0]}, {table[4][1]}, {table[4][2]}, {table[4][3]}, {table[4][4]}, {table[4][5]}, {table[4][6]}, {table[4][7]}
-        
-        Character 6:
-        {table[5][0]}, {table[5][1]}, {table[5][2]}, {table[5][3]}, {table[5][4]}, {table[5][5]}, {table[5][6]}, {table[5][7]}
-        
-        Character 7:
-        {table[6][0]}, {table[6][1]}, {table[6][2]}, {table[6][3]}, {table[6][4]}, {table[6][5]}, {table[6][6]}, {table[6][7]}
-        
-        Character 8:
-        {table[7][0]}, {table[7][1]}, {table[7][2]}, {table[7][3]}, {table[7][4]}, {table[7][5]}, {table[7][6]}, {table[7][7]}
-        
-        Character 9:
-        {table[8][0]}, {table[8][1]}, {table[8][2]}, {table[8][3]}, {table[8][4]}, {table[8][5]}, {table[8][6]}, {table[8][7]}
-        
-        Character 10:
-        {table[9][0]}, {table[9][1]}, {table[9][2]}, {table[9][3]}, {table[9][4]}, {table[9][5]}, {table[9][6]}, {table[9][7]}
-        '''
+        output = ''
+        for i in range(10):
+            character = ''
+            character += f'Игрок {i+1} ' \
+                         f'Характеристики: \n'
+            for a in range(8):
+                if a != 0:
+                    character += ', '
+                number_of_feature = int(table[i][a])
+                try:
+                    character += f'{features[a][number_of_feature]}'
+                except IndexError:
+                    print('list index out of range')
+            character += '\n\n'
+            output += character
+
+        self.__restore_table()
+
         return output
